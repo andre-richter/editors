dba8936a5612c76eb8102fc9ed2e30602027a43b
Andre Richter <andre.o.richter@gmail.com>
2017-10-18 09:57:07 +0200

    Go back to normal mouse scroll. Make CTRL+Backspace backward delete word

--- init.el
+++ init.el
@@ -3,6 +3,20 @@
   (normal-top-level-add-to-load-path '("."))
   (normal-top-level-add-subdirs-to-load-path))
 
+;; In emacs nw mode, CTRL+Backspace produces C-h.  Also, make it
+;; delete, not kill, the last word.
+(defun my-delete-word (arg)
+  (interactive "p")
+  (delete-region
+   (point)
+   (progn
+     (forward-word arg)
+     (point))))
+(defun backward-delete-word (arg)
+  (interactive "p")
+  (my-delete-word (- arg)))
+(global-set-key (kbd "C-h") 'backward-delete-word)
+
 ;; Font size
 (set-face-attribute 'default nil :height 140)
 
@@ -24,23 +38,24 @@
 
 (setq mouse-wheel-follow-mouse 't)
 
-(defvar alternating-scroll-down-next t)
-(defvar alternating-scroll-up-next t)
+;; Scroll only a single line per Mouse wheel turn
+;; (defvar alternating-scroll-down-next t)
+;; (defvar alternating-scroll-up-next t)
 
-(defun alternating-scroll-down-line ()
-  (interactive "@")
-  (when alternating-scroll-down-next
-    (scroll-down-line))
-  (setq alternating-scroll-down-next (not alternating-scroll-down-next)))
+;; (defun alternating-scroll-down-line ()
+;;   (interactive "@")
+;;   (when alternating-scroll-down-next
+;;     (scroll-down-line))
+;;   (setq alternating-scroll-down-next (not alternating-scroll-down-next)))
 
-(defun alternating-scroll-up-line ()
-  (interactive "@")
-  (when alternating-scroll-up-next
-    (scroll-up-line))
-  (setq alternating-scroll-up-next (not alternating-scroll-up-next)))
+;; (defun alternating-scroll-up-line ()
+;;   (interactive "@")
+;;   (when alternating-scroll-up-next
+;;     (scroll-up-line))
+;;   (setq alternating-scroll-up-next (not alternating-scroll-up-next)))
 
-(global-set-key (kbd "<mouse-4>") 'alternating-scroll-down-line)
-(global-set-key (kbd "<mouse-5>") 'alternating-scroll-up-line)
+;; (global-set-key (kbd "<mouse-4>") 'alternating-scroll-down-line)
+;; (global-set-key (kbd "<mouse-5>") 'alternating-scroll-up-line)
 
 ;; No startup message
 (setq inhibit-startup-message t)
