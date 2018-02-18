9c7b3c08b1739202dce614b652d8f7378e0ad17b
Andre Richter <andre.o.richter@gmail.com>
2018-02-18 13:06:10 +0100

    reorder emacs check and file linking

--- install.sh
+++ install.sh
@@ -20,16 +20,19 @@
 ################
 # Emacs
 ################
-mkdir ~/.emacs.d
-ln -s ~/repos/editors/init.el ~/.emacs.d/init.el
-mkdir ~/.emacs.d/lisp
-
 echo ""
 
 if ! [[ -x "$(command -v emacs)" ]]; then
   echo "emacs not found. Is it installed?" >&2
   exit
 fi
+
+rm -rf ~/.emacs.d
+rm ~/.emacs.d/init.el
+
+mkdir ~/.emacs.d
+ln -s ~/repos/editors/init.el ~/.emacs.d/init.el
+mkdir ~/.emacs.d/lisp
 
 echo "Install dependencies:"
 
